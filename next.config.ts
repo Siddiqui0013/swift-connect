4acf1155194dd1dad6e0a9aaf35ee4ee14fc4141
siddiqui0013 <asifm8702@gmail.com>
2025-03-12 15:51:43 +0500

    img

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,16 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    domains: ['swift-connect-mu.vercel.app/'], // Add your Vercel domain here
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**',
+      },
+    ],
+  },
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/' : '',
 };
 
 export default nextConfig;
